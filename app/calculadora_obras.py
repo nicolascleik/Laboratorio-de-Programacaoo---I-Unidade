8c71feaa9361d4781f81d55b38386570dbe777cb
Nicolas Cleik <nicolas@cleik.dev>
2026-03-24 15:25:22 -0300

    Adição das docstrings

--- app/calculadora_obras.py
+++ app/calculadora_obras.py
@@ -4,6 +4,17 @@
 # ==========================================
 
 def calcular_area_em_m2(altura, comprimento):
+    """
+    Calcula a área total de uma superfície em metros quadrados (m²).
+
+    Parâmetros:
+        altura (float): A medida da altura da superfície.
+        comprimento (float): A medida do comprimento da superfície.
+
+    Retorno:
+        float: O resultado da multiplicação da altura pelo comprimento.
+    """
+
     try:
         altura = float(altura)
         comprimento = float(comprimento)
@@ -18,6 +29,17 @@ def calcular_area_em_m2(altura, comprimento):
     return area_m2
 
 def calcular_tijolos_por_parede(area_parede_m2, area_tijolo_m2):
+    """
+    Calcula a quantidade estimada de tijolos necessários para preencher uma parede.
+
+    Parâmetros:
+        area_parede_m2 (float): A área total da parede em metros quadrados.
+        area_tijolo_m2 (float): A área que um único tijolo ocupa em metros quadrados.
+
+    Retorno:
+        float: A quantidade de tijolos (blocos) necessários, arredondada para duas casas decimais.
+    """
+    
     try:
         area_parede_m2 = float(area_parede_m2)
         area_tijolo_m2 = float(area_tijolo_m2)
