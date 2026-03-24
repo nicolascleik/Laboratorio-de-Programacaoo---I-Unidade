c562c2eec776f01d345222b855f3f1c0cf980228
Nicolas Cleik <nicolas@cleik.dev>
2026-03-24 14:04:42 -0300

    Finalização da logica do lançamento de erros (NÃO HOUVE A APLICAÇÃO DO PYTEST AINDA)

--- app/calculadora_obras.py
+++ app/calculadora_obras.py
@@ -1,9 +1,28 @@
 def calcular_area_em_m2(altura, comprimento):
+    try:
+        altura = float(altura)
+        comprimento = float(comprimento)
+    except ValueError:
+        raise TypeError("O valor informado deve ser um número válido e não uma letra.")
+
+    if altura <= 0 or comprimento <= 0:
+        raise ValueError("Os valores de ALTURA e COMPRIMENTO devem ser maiores que zero")
+
     area_m2 = altura * comprimento
 
     return area_m2
 
 def calcular_tijolos_por_parede(area_parede_m2, area_tijolo_m2):
+    try:
+        area_parede_m2 = float(area_parede_m2)
+        area_tijolo_m2 = float(area_tijolo_m2)
+    except ValueError:
+        raise TypeError("O valor informado deve ser um número válido e não uma letra.")
+
+    if area_parede_m2 <= 0 or area_tijolo_m2 <= 0:
+        raise ValueError("Os valores de AREA_PAREDE e AREA_TIJO devem ser maiores que zero! A função 'calcular_area_em_m2(altura, comprimento)' vai te ajudar entregando o resultado final em m2")
+
+
     quantidade_de_blocos = round((area_parede_m2 / area_tijolo_m2), 2)
 
     return quantidade_de_blocos
